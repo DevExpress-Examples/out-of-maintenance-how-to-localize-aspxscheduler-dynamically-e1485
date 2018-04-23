8a06b8da52c1c08e4f2284eac6b94a13252ff7f5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<%@ Register assembly="DevExpress.Web.ASPxScheduler.v15.2, Version=15.2.0.0, Culture=neutral, PublicKeyToken=79868b8147b5eae4" namespace="DevExpress.Web.ASPxScheduler" tagprefix="dxwschs" %>
+<%@ Register assembly="DevExpress.XtraScheduler.v15.2.Core, Version=15.2.0.0, Culture=neutral, PublicKeyToken=79868b8147b5eae4" namespace="DevExpress.XtraScheduler" tagprefix="dxschsc" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    <dxwschs:ASPxScheduler ID="ASPxScheduler1" runat="server">
+    </dxwschs:ASPxScheduler>
+    </form>
+</body>
+</html>
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,60 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+#Region "#usings_MyLocalizer"
+Imports DevExpress.Web.ASPxScheduler.Localization
+Imports DevExpress.Utils.Localization.Internal
+Imports DevExpress.XtraScheduler.Localization
+#End Region ' #usings_MyLocalizer
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+ #Region "#PageInit"
+    Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+        Dim myLocalizer As New MyLocalizer()
+        Dim provider As New DefaultActiveLocalizerProvider(Of ASPxSchedulerStringId)(myLocalizer)
+        ASPxSchedulerLocalizer.SetActiveLocalizerProvider(provider)
+        ASPxSchedulerLocalizer.Active = myLocalizer
+
+        Dim myLocalizerCore As New MyLocalizerCore()
+        Dim providerCore As New DefaultActiveLocalizerProvider(Of SchedulerStringId)(myLocalizerCore)
+        SchedulerLocalizer.SetActiveLocalizerProvider(providerCore)
+        SchedulerLocalizer.Active = myLocalizerCore
+    End Sub
+#End Region ' #PageInit
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+    End Sub
+#Region "#MyLocalizer"
+    Public Class MyLocalizer
+        Inherits DevExpress.Web.ASPxScheduler.Localization.ASPxSchedulerLocalizer
+
+        Public Overrides Function GetLocalizedString(ByVal id As ASPxSchedulerStringId) As String
+            Dim ret As String = ""
+            Select Case id
+                Case DevExpress.Web.ASPxScheduler.Localization.ASPxSchedulerStringId.CaptionViewNavigator_Today
+                    Return Date.Now.Date.ToShortDateString()
+                Case Else
+                    ret = MyBase.GetLocalizedString(id)
+            End Select
+            Return ret
+        End Function
+    End Class
+
+    Public Class MyLocalizerCore
+        Inherits DevExpress.XtraScheduler.Localization.SchedulerLocalizer
+
+        Public Overrides Function GetLocalizedString(ByVal id As DevExpress.XtraScheduler.Localization.SchedulerStringId) As String
+            If id = DevExpress.XtraScheduler.Localization.SchedulerStringId.MenuCmd_NewAppointment Then
+                Return "New Event"
+            End If
+
+            Return MyBase.GetLocalizedString(id)
+        End Function
+    End Class
+#End Region ' #MyLocalizer
+End Class
